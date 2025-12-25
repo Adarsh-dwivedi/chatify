2db4d52b3206c0015d8dd5638aa7781e586b8fae
Adarsh Dwivedi <adarsh13dwived@gmail.com>
2025-12-25 15:12:00 +0530

    Login and Logout functionality implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,3 +64,35 @@ export const signup = async (req, res)=>{
     
 
 };
+
+export const login = async (req, res)=>{
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({message: "Email and password are required"});
+    }
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: "Invalid credential"});
+
+        const isCorrectPassword = await bcrypt.compare(password, user.password);
+        if(!isCorrectPassword) return res.status(400).json({message: "Invalid credential"});
+
+        generateTokenAndAddToCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        });
+
+    }catch(error){
+        console.error("Login faied: ", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export const logout =  (req, res)=>{
+    res.cookie("token", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out successfuly"});
+}
